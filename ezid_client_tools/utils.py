75352d5bff01d98e7646d28843c5d2b55c822b16
Raymond Yee <raymond.yee@gmail.com>
2023-05-12 12:00:17 -0700

    changing methods of utils.ANVL to static methods

--- ezid_client_tools/utils.py
+++ ezid_client_tools/utils.py
@@ -21,14 +21,13 @@ class ANVL:
 
     """
 
-    def __init__(self):
-        pass
-
-    def escape_anvl(self, str_val):
+    @staticmethod
+    def escape_anvl(str_val):
         """makes ANVL safe strings by escaping certain values"""
         return re.sub("[%:\r\n]", lambda c: "%%%02X" % ord(c.group(0)), str_val)
 
-    def parse_anvl_str(self, anvl):
+    @staticmethod
+    def parse_anvl_str(anvl):
         """parse an anvl string into a dictionary object of metadata"""
         metadata = LastUpdatedOrderedDict()
         for anvl_line in anvl.decode("UTF-8").splitlines():
@@ -37,13 +36,14 @@ class ANVL:
                 if len(line_ex) > 1:
                     esc_key = line_ex[0].strip()
                     esc_value = line_ex[1].strip()
-                    key = self.unescape_anvl(esc_key).strip()
-                    value = self.unescape_anvl(esc_value).strip()
+                    key = ANVL.unescape_anvl(esc_key).strip()
+                    value = ANVL.unescape_anvl(esc_value).strip()
                     if len(key) > 0 and len(value) > 0:
                         metadata[key] = value
         return metadata
 
-    def unescape_anvl(self, str_val):
+    @staticmethod
+    def unescape_anvl(str_val):
         """unescapes an escaped ANVL string"""
         return re.sub(
             "%([0-9A-Fa-f][0-9A-Fa-f])", lambda m: chr(int(m.group(1), 16)), str_val
